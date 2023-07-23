180a883792caf5a1edd39703ef717f8feda6f145
sherzobek <115620993+sherzobek@users.noreply.github.com>
2023-07-23 15:25:20 +0300

    fist commit

---
+++ js-praktika/app.js
@@ -0,0 +1,137 @@
+"use strict"
+// 4 darst data types yani malumotlar turlari
+// number
+// number = 10;
+// number2 = 9;
+// number3 = 10;
+// console.log(number2 * number3);
+// console.log(number);
+
+// // string
+// const cleantName = "sherzodbek";
+
+
+// //boolean
+// const isMaried = false;
+// const isCricle = true;
+// console.log(isCricle)
+
+// //undefined
+// let und;
+// console.log(und)
+ 
+
+// //object
+//  const thief = {
+//    age:30,
+//    jacket:"black",
+//    isLong:false,}
+
+//   console.log(thief.jacket)
+
+//   const Navroza = {
+//    sochi:"qora",
+   // kozi: " qora,kotta ",
+   // islonger:false,}
+
+//   console.log(Navroza.islonger)
+
+  //array
+   // const colors = ["red","blue","black"]
+   // console.log(colors[0]);
+
+   // const brothers = ["sherzod" ,"begzod" ,"muhammadZiyo" ,10, [FileSystemWritableFileStream],[], {}]
+   // console.log(brothers[4]);
+
+   // 5dars foydalanuvchi bilan muloqot
+   
+   // const place = confirm("sen shu yerdamisan?");
+   // console.log(place);
+   
+   // const myPlace = confirm("senga yoqmoqdami javacriptda cod yozish?");
+   // console.log(myPlace);
+
+   // const myanswer = confirm("Sen uylanganmisan? Yoki turmush qurganmisan?");
+   // console.log(myanswer);
+
+   // const myPrompt = prompt("sen qayerdansan ?");
+   // console.log(myPrompt); // + siz berilgan qiymatlar string bolib chiqadi
+
+   // let myPrompt3 = prompt("ozingning qizishlaring haqida gapirishni hohlaysanmi? Unda yozaver");
+   // console.log(myPrompt3 +  "Sen menga yoqib qolding");
+    
+
+   // let myPrompt2 = +prompt("20 soning 20 darajasi qancha boladi?");
+   // console.log(myPrompt2)// prompt oldidagi + bu natijani number qilish uchun ishalatiladi
+
+   // let myTest = +prompt("2 ni 2 ga kopaytiring va chiqqan sonni yozing?");
+   // console.log(myTest + " Sen juda ham topqir ekansan");
+    
+   // massivni propmt orqali ishlatish
+   // const favouriteColors = [];
+   //  favouriteColors[0] = prompt("sizning yoqtirgan rangingiz #0","");
+   //  favouriteColors[1] = prompt("sizning yoqtirgan rangingiz #1","");
+   //  favouriteColors[2] = prompt("sizning yoqtirgan rangingiz #","");
+   // console.log(favouriteColors);
+
+  // let myHobbis = [];
+  //    myHobbis[0] = prompt('siz nimalarga qiziqasiz' ,"");
+  //    myHobbis[1] = prompt('siz nimalarga ishtiyoqingiz bor',"");
+  //    myHobbis[2] = prompt('siz nimalarga qiziqasiz',"");
+
+  //    console.log(myHobbis);
+      
+    //  const Mypractica = [];
+    // Mypractica[0] = +prompt("2 ning 3-darajasi nechiga teng?");
+    // Mypractica[1] = +prompt("3 ning 4-darajasi nechiga teng?");
+
+    // console.log( typeof Mypractica);
+
+     
+                  // 2023.07.19  6-darst Interpolatsiya haqida boshlangiz malumot
+
+// const channal = prompt("what's your favourite youtuber");
+// console.log("https://youtube.com/" + channal + "/videos"); bu eski turdagi kod manbai
+
+// const channal = prompt("siz yoqtirgan blogeringiz?");
+// console.log(`https://youtube.com/${channal}`);
+
+               //2023.07.22 7-darst operatorlar
+
+// console.log("sherzodbek" + "Abdumutalov");//string bilan sitring qoshish mumkin
+
+// let incr = 10;
+// let decr = 10;
+
+// console.log(incr++); // inkrement sonni biita kop qoshib berdi
+// console.log(decr--);// dikriment sonni biita kAMAYTIRIB BERRADI
+
+// console.log(incr++);
+// console.log(decr--);
+
+// console.log(incr++);
+// console.log(decr--);
+
+// console.log(incr++);
+// console.log(decr--);
+
+// console.log(incr++);
+// console.log(decr--);
+
+// console.log(5 * 5 == 25);//tru yani ==  operatori tru javobini beradi
+ 
+//&& = va  // || = YOKI
+ 
+// const isAge = true;
+// const isClose = true;
+// const isChecked = false;
+
+// console.log( isAge || isClose || isChecked); //yani ||- bu har doim  kodning ichida true nin chiqarib beradi agarda 10 ta false ichida bitta  bolsa ham
+// console.log(isAge && isClose && isChecked); // yani &&- bu har doim false  ni chiqarib  beradi 10 ta true bolsa ham
+
+                    // 2023.07.23
+                   // 8-darst  github bilan ishalash
+                   c
+
+  
+   
